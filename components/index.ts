aed5dbd65ff97118bc303cfeaeadbf33feffe50c
landerson98n <landerson98n@gmail.com>
2023-08-30 13:35:10 -0300

    update

--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,6 @@
 import { InitialPage } from "./InitialPage";
 import { Login } from "./Login";
 import { Register } from "./Register";
-import { Dashboard } from "./Dashboard"; 
 
-export {InitialPage, Login,Register, Dashboard}
+
+export {InitialPage, Login,Register}
